39a4d786b7853e6563b7f531a3a79603840986d5
Bunty9 <cbipin2000@gmail.com>
2021-12-30 16:08:35 +0530

    follow/unfollow

--- middleware/verifyAuth.js
+++ middleware/verifyAuth.js
@@ -1,19 +1,19 @@
 const jwt = require("jsonwebtoken");
 
 verifyToken = (req, res, next) => {
-	let token = req.headers["x-access-token"];
+    let token = req.headers["x-access-token"];
 
-	if (!token) {
-		return res.status(403).send({ message: "No token provided!" });
-	}
+    if (!token) {
+        return res.status(403).send({ message: "No token provided!" });
+    }
 
-	jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-		if (err) {
-			return res.status(401).send({ message: "Unauthorized!" });
-		}
-		req.userId = decoded.id;
-		next();
-	});
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "Unauthorized!" });
+        }
+        req.userId = decoded.id;
+        next();
+    });
 };
 
 module.exports = verifyToken;
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -6,11 +6,64 @@ const router = express.Router();
 //getUserInfo
 //editUserInfo
 // follow
+router.post("/:id/follow", verifyToken, async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+        return next({
+            message: `No user found for id ${req.params.id}`,
+            statusCode: 404,
+        });
+    }
+    // check if already following
+    if (user.followers.includes(req.userId)) {
+        return next({ message: "You are already following him", status: 400 });
+    }
+    await User.findByIdAndUpdate(req.params.id, {
+        $push: { followers: req.userId },
+        $inc: { followersCount: 1 },
+    });
+    await User.findByIdAndUpdate(req.userId, {
+        $push: { following: req.params.id },
+        $inc: { followingCount: 1 },
+    });
+
+    res.status(200).json({ success: true, data: {} });
+});
+
 // unfollow
+router.post("/:id/unfollow", async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        return next({
+            message: `No user found for ID ${req.params.id}`,
+            statusCode: 404,
+        });
+    }
+    if (req.params.id === req.userId) {
+        return next({
+            message: "You can't follow/unfollow yourself",
+            status: 400,
+        });
+    }
+    await User.findByIdAndUpdate(req.params.id, {
+        $pull: { followers: req.userId },
+        $inc: { followersCount: -1 },
+    });
+    await User.findByIdAndUpdate(req.userId, {
+        $pull: { following: req.params.id },
+        $inc: { followingCount: -1 },
+    });
+
+    res.status(200).json({ success: true, data: {} });
+});
+
 //getTweets
 
+//test route
 router.route("/verify").post(verifyToken, (req, res) => {
-	res.send("authorized");
+    console.log(req.userId);
+    res.send("authorized");
 });
 
 // exports.feed = asyncHandler(async (req, res, next) => {
@@ -18,7 +71,7 @@ router.route("/verify").post(verifyToken, (req, res) => {
 
 // 	const users = await User.find()
 // 		.where("_id")
-// 		.in(following.concat([req.user.id]))
+// 		.in(following.concat([req.userId]))
 // 		.exec();
 
 // 	const tweetIDS = users.map((user) => user.tweet).flat();
